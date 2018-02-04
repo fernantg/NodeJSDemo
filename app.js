ce7f7400bd82b8eddc90fb41931141780623512d
root <root@ip-172-30-0-146.ap-southeast-1.compute.internal>
2018-02-04 15:42:10 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+	res.setHeader('content-type', 'text/application');
+	res.send('Node.js App');
+	console.log('Access from ' + ip);
+});
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!');
+});
